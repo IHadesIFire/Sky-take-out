368344bd9245cbbef1677f0fdb7746df4964e62c
14462 <13919936370@163.com>
2025-11-28 12:31:35 +0800

    initiated code

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -1,9 +1,12 @@
 package com.sky.service;
 
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 
 public interface EmployeeService {
+
+    public void save(EmployeeDTO employeeDTO);
 
     /**
      * 员工登录
